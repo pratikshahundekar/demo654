2b740f4874c9ae3721fb8b6818d21cb9a7426a30
hp <hp@LAPTOP-Q990A920>
2022-05-06 14:32:53 +0530

    pratiksha created demo1 class..

--- src/main/java/Pratiksha/Demo1.java
+++ src/main/java/Pratiksha/Demo1.java
@@ -6,6 +6,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 
 		System.out.println("Demo1 class added by Pratiksha");
+		System.out.println("Demo1 created by pratiksha");
 	}
 
 }
