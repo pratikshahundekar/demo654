0b758ecc1aa80a13e27d63bcb2529db21cd89d32
hp <hp@LAPTOP-Q990A920>
2022-05-06 14:18:21 +0530

    Pratiksha commit 2nd Demo2 class

--- src/main/java/Pratiksha/Demo1.java
+++ src/main/java/Pratiksha/Demo1.java
@@ -1,10 +1,11 @@
 package Pratiksha;
 
+
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Demo1 class added by Pratiksha");
 	}
 
 }
---
+++ src/main/java/Pratiksha/Demo2.java
@@ -0,0 +1,11 @@
+package Pratiksha;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Demo2 class test case 1 created by Pratiksha ");
+		System.out.println("Demo2 class test case 2 created by Pratiksha ");
+	}
+
+}
