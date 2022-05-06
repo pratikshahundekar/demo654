596b51b92e11f990e865cd5f41746a94348eb96b
hp <hp@LAPTOP-Q990A920>
2022-05-06 15:31:41 +0530

    pratiksha 2nd commit..

---
+++ src/main/java/Pratiksha/Tutorial2.java
@@ -0,0 +1,11 @@
+package Pratiksha;
+
+public class Tutorial2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Tutorial2 created by pratiksha");
+		System.out.println("pratiksha created tutorial");
+	}
+
+}
